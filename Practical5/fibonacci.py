b1c1fc8dabf1bca7fb37f57343b21cff25087591
3200110888bit <3200110888@zju.edu.cn>
2021-03-10 02:36:09 +0000

    fibonacci

--- Practical5/fibonacci.py
+++ Practical5/fibonacci.py
@@ -1,10 +1,10 @@
 #set the first two variables as 1, 1
-n1=1
-n2=1
+a=1
+b=1
 #repeat adding the previous two numbers, use the sum of the previous two number$
 for i in range (3,14):
-        n3=n1+n2
-        n1=n2
-        n2=n3
+        c=a+b
+        a=b
+        b=c
         i=i+1
 print n3
