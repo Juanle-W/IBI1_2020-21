f32c583ff642b1f3046499ab2df558b06fda55b3
3200110888bit <3200110888@zju.edu.cn>
2021-03-10 07:48:20 +0000

    fibonacci

--- Practical5/fibonacci.py
+++ Practical5/fibonacci.py
@@ -1,7 +1,7 @@
 #set the first two variables as 1, 1
 a=1
 b=1
-#repeat adding the previous two numbers, use the sum of the previous two number as the next number, until getting the 13th value
+#repeat adding the previous two numbers, use the sum of the previous two number as the next number, since a represent two numbers before the number I want, I should replace a value with the current b value. Also I should replace b value with the current c value. Finally I get the 13th value and show it as c value.
 for i in range (3,14):
         c=a+b
         a=b
