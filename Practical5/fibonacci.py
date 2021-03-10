a0e1bf23d42fb02e070ed0d2b36e96f53dc94276
3200110888bit <3200110888@zju.edu.cn>
2021-03-10 02:41:24 +0000

    fibonacci

--- Practical5/fibonacci.py
+++ Practical5/fibonacci.py
@@ -1,7 +1,7 @@
 #set the first two variables as 1, 1
 a=1
 b=1
-#repeat adding the previous two numbers, use the sum of the previous two number$
+#repeat adding the previous two numbers, use the sum of the previous two number as the next number, until getting the 13th value
 for i in range (3,14):
         c=a+b
         a=b
