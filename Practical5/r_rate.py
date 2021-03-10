0d94c8f82b27f76205c5e7c6655828ff1ae25176
3200110888bit <3200110888@zju.edu.cn>
2021-03-10 07:50:16 +0000

    r_rate

--- Practical5/r_rate.py
+++ Practical5/r_rate.py
@@ -1,4 +1,4 @@
-#repeat multiplying the number of IBI1 students by r rate for 5 times by use for-loop, get the final number
+#repeat multiplying the number of IBI1 students by r rate for 5 times by using for-loop, get the final number, and print the number as a string
 n=84
 r=1.2
 for i in range (1,6):
