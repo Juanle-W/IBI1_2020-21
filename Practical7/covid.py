de037f73310e116d81156289e1f53f560a1f7fac
user <user@userdeMacBook-Air.local>
2021-03-22 23:50:02 +0800

    covid

--- Practical7/covid.py
+++ Practical7/covid.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-os.chdir("Users/user/Downloads")
+os.chdir("/Users/user/Downloads")
 os.getcwd()
 os.listdir()
 covid_data=pd.read_csv("full_data.csv")
