167f7a4e15c239ab24974c09cee3469a16cbe8e0
user <user@userdeMacBook-Air.local>
2021-03-31 19:59:42 +0800

    unknown function

--- Practical8/get_unknown_function.py
+++ Practical8/get_unknown_function.py
@@ -1,6 +1,7 @@
-import re
+#input and output fa files
 f = open('/Users/user/IBI1_2020-21/Practical8/Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa')
 fw = open('/Users/user/IBI1_2020-21/Practical8/unknown_function.fa','w')
+#read the Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa file
 line = f.readlines()
 seq = []
 name = []
@@ -8,16 +9,18 @@ for i in range (len(line)):
 	if line[i].startswith('>') and line[i].find('unknown function')>=0:
 		name.append(line[i].split()[0].split('_')[0])
 	elif line[i-1].startswith('>') and line[i-1].find('unknown function')>=0:
-		partseq = ''
+		partseq = '' #prevent accumulating of the sequences from different genes 
+#try to get all lines that have information about the DNA sequence of one gene
+#use for loop and if loop to get which lines have sequence information
 		for j in range (i,len(line)):
 			if line[j].startswith('>'):
 				break
 			else:
-				partseq += line[j].replace('\n','').strip()
-		seq.append(partseq)			
-print(name)
+				partseq += line[j].replace('\n','').strip() #store all information from different lines in one string called partseq
+		seq.append(partseq) #put the sequence of one gene into the list
+print(name) #check if the loop works normally
 f.close()
-
+#write the output file
 for i in range (len(name)):
 	fw.write(name[i])
 	fw.write(' ')
