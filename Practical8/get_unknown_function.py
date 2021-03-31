5704a6c1de8b1a5490fc138b96f3bf2766d20ce7
user <user@userdeMacBook-Air.local>
2021-03-31 16:20:06 +0800

    unknown function

--- Practical8/get_unknown_function.py
+++ Practical8/get_unknown_function.py
@@ -1,11 +1,20 @@
-function = open('/Users/user/IBI1_2020-21/Practical8/Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa')
+import re
+f = open('/Users/user/IBI1_2020-21/Practical8/Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa')
+fw = open('/Users/user/IBI1_2020-21/Practical8/unknown_function.fa','w')
 seq = {}
-for line in function:
+for line in f:
 	if line.startswith('>'):
-		name = line.replace('>','').split()[0]
+		name = line.split()[0].split('_')[0]
 		seq[name] = ''
 	else:
 		seq[name] += line.replace('\n','').strip()
 
-print(seq[name])
-function.close()
+f.close()
+
+for i in  seq.keys():
+	fw.write(i)
+	fw.write(' ')
+	fw.write(str(len(seq[i])))
+	fw.write('\n')
+
+f.close()
