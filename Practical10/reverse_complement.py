abbbf29de8c71d27e6b0c4c5b221210624b7dd9d
user <user@userdeMacBook-Air.local>
2021-04-14 12:56:35 +0800

    reverse complement

--- Practical10/reverse_complement.py
+++ Practical10/reverse_complement.py
@@ -1,14 +1,15 @@
+#make a function
 def reverse_complement (seq):
 	abab={'A':'T', 'T':'A', 'C':'G', 'G':'C', 'c':'g', 'g':'c', 'a':'t', 't':'a'}
-	bases=list(seq)
-	complement_bases=[]
-	reverse_complement_bases=[]
+	bases=list(seq)#change the string type into the list type
+	complement_bases=[]#build a list to store the complement bases
+	reverse_complement_bases=[]#build a list to store the reverse complement bases
 	for i in range(len(bases)):
-		complement_bases.append(abab[bases[i]])
+		complement_bases.append(abab[bases[i]])#to get the complement bases
 	for j in range(len(bases)):
-		reverse_complement_bases.append(complement_bases[len(bases)-j-1])
+		reverse_complement_bases.append(complement_bases[len(bases)-j-1])#to reverse the complement bases
 	print('The reverse complement of a DNA strand is', end=' ')
-	result=''.join(reverse_complement_bases)
+	result=''.join(reverse_complement_bases)#change the list type to the string type
 	return print(result)
 
 reverse_complement('ATCGGGTCag')
