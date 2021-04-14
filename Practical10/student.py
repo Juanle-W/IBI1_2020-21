1e3154f9d9a77cb9d6fc02380ea9bfb443491e1b
user <user@userdeMacBook-Air.local>
2021-04-14 13:10:17 +0800

    student

--- Practical10/student.py
+++ Practical10/student.py
@@ -5,7 +5,7 @@ class Student(object):
 		self.last_name=last_name
 		self.programme=programme
 	def __str__(self):#to output the student's full name and undergraduate programme
-		return "full name: %s %s; undergraduate programme:  %s"%(self.first_name,self.last_name,self.programme)
+		return "full name: %s %s; undergraduate programme:  %s"%(self.first_name,self.last_name,self.programme)#I got the idea from "https://blog.csdn.net/pdstar/article/details/90900944"
 
 #give an instance
 a=Student('Wang', 'Juanle', 'BMS')
