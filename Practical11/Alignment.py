2d2a66c003989a6b1da8ee87c4329f616c797bfd
user <user@userdeMacBook-Air.local>
2021-04-21 15:24:06 +0800

    Alignment

---
+++ Practical11/Alignment.py
@@ -0,0 +1,74 @@
+f1 = open('/Users/user/Downloads/SOD2_human.fa')
+f2 = open('/Users/user/Downloads/SOD2_mouse.fa')
+f3 = open('/Users/user/Downloads/RandomSeq.fa')
+line1 = f1.readlines()
+line2 = f2.readlines()
+line3 = f3.readlines()
+SOD2_human=''
+SOD2_mouse=''
+RandomSeq=''
+for i in range (len(line1)):
+	if not line1[i].startswith('>'):
+		SOD2_human += line1[i].replace('\n','').strip()
+
+for i in range (len(line2)):
+	if not line2[i].startswith('>'):
+		SOD2_mouse += line2[i].replace('\n','').strip()
+
+for i in range (len(line3)):
+	if not line3[i].startswith('>'):
+		RandomSeq += line3[i].replace('\n','').strip()
+
+print(SOD2_human)
+print(SOD2_mouse)
+print(RandomSeq)
+f1.close()
+f2.close()
+f3.close()
+amino = ['A', 'R', 'N', 'D', 'C', 'Q', 'E', 'G', 'H', 'I', 'L', 'K', 'M', 'F', 'P', 'S', 'T', 'W', 'Y', 'V', 'B', 'Z', 'X', '*']
+blosum = [
+[ 4, -1, -2, -2,  0, -1, -1,  0, -2, -1, -1, -1, -1, -2, -1,  1,  0, -3, -2,  0, -2, -1,  0, -4],
+[-1,  5,  0, -2, -3,  1,  0, -2,  0, -3, -2,  2, -1, -3, -2, -1, -1, -3, -2, -3, -1,  0, -1, -4],
+[-2,  0,  6,  1, -3,  0,  0,  0,  1, -3, -3,  0, -2, -3, -2,  1,  0, -4, -2, -3,  3,  0, -1, -4],
+[-2, -2,  1,  6, -3,  0,  2, -1, -1, -3, -4, -1, -3, -3, -1,  0, -1, -4, -3, -3,  4,  1, -1, -4],
+[ 0, -3, -3, -3,  9, -3, -4, -3, -3, -1, -1, -3, -1, -2, -3, -1, -1, -2, -2, -1, -3, -3, -2, -4],
+[-1,  1,  0,  0, -3,  5,  2, -2,  0, -3, -2,  1,  0, -3, -1,  0, -1, -2, -1, -2,  0,  3, -1, -4],
+[-1,  0,  0,  2, -4,  2,  5, -2,  0, -3, -3,  1, -2, -3, -1,  0, -1, -3, -2, -2,  1,  4, -1, -4],
+[ 0, -2,  0, -1, -3, -2, -2,  6, -2, -4, -4, -2, -3, -3, -2,  0, -2, -2, -3, -3, -1, -2, -1, -4],
+[-2,  0,  1, -1, -3,  0,  0, -2,  8, -3, -3, -1, -2, -1, -2, -1, -2, -2,  2, -3,  0,  0, -1, -4],
+[-1, -3, -3, -3, -1, -3, -3, -4, -3,  4,  2, -3,  1,  0, -3, -2, -1, -3, -1,  3, -3, -3, -1, -4],
+[-1, -2, -3, -4, -1, -2, -3, -4, -3,  2,  4, -2,  2,  0, -3, -2, -1, -2, -1,  1, -4, -3, -1, -4],
+[-1,  2,  0, -1, -3,  1,  1, -2, -1, -3, -2,  5, -1, -3, -1,  0, -1, -3, -2, -2,  0,  1, -1, -4],
+[-1, -1, -2, -3, -1,  0, -2, -3, -2,  1,  2, -1,  5,  0, -2, -1, -1, -1, -1,  1, -3, -1, -1, -4],
+[-2, -3, -3, -3, -2, -3, -3, -3, -1,  0,  0, -3,  0,  6, -4, -2, -2,  1,  3, -1, -3, -3, -1, -4],
+[-1, -2, -2, -1, -3, -1, -1, -2, -2, -3, -3, -1, -2, -4,  7, -1, -1, -4, -3, -2, -2, -1, -2, -4],
+[ 1, -1,  1,  0, -1,  0,  0,  0, -1, -2, -2,  0, -1, -2, -1,  4,  1, -3, -2, -2,  0,  0,  0, -4],
+[ 0, -1,  0, -1, -1, -1, -1, -2, -2, -1, -1, -1, -1, -2, -1,  1,  5, -2, -2,  0, -1, -1,  0, -4],
+[-3, -3, -4, -4, -2, -2, -3, -2, -2, -3, -2, -3, -1,  1, -4, -3, -2, 11,  2, -3, -4, -3, -2, -4],
+[-2, -2, -2, -3, -2, -1, -2, -3,  2, -1, -1, -2, -1,  3, -3, -2, -2,  2,  7, -1, -3, -2, -1, -4],
+[ 0, -3, -3, -3, -1, -2, -2, -3, -3,  3,  1, -2,  1, -1, -2, -2,  0, -3, -1,  4, -3, -2, -1, -4],
+[-2, -1,  3,  4, -3,  0,  1, -1,  0, -3, -4,  0, -3, -3, -2,  0, -1, -4, -3, -3,  4,  1, -1, -4],
+[-1,  0,  0,  1, -3,  3,  4, -2,  0, -3, -3,  1, -1, -3, -1,  0, -1, -3, -2, -2,  1,  4, -1, -4],
+[ 0, -1, -1, -1, -2, -1, -1, -1, -1, -1, -1, -1, -1, -1, -2,  0,  0, -2, -1, -1, -1, -1, -1, -4],
+[-4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4,  1],
+]
+
+#compare different sequences
+def alignment(s,t,score):
+	for i in range (len(s)):
+		score=score+blosum[amino.index(s[i])][amino.index(t[i])]
+	return score
+
+print('the score between human and mouse is '+str(alignment(SOD2_human,SOD2_mouse,0)))
+print('the score between human and random sequence is '+str(alignment(SOD2_human,RandomSeq,0)))
+print('the score between mouse and random sequence is '+str(alignment(SOD2_mouse,RandomSeq,0)))
+#get the percentage of identical amino acids for each comparison
+def percent(s,t,number):
+	for i in range (len(s)):
+		if s[i]==t[i]:
+			number=number+1
+	return number/len(s)*100
+
+print(percent(SOD2_human,SOD2_mouse,0))
+print(percent(SOD2_human,RandomSeq,0))
+print(percent(SOD2_mouse,RandomSeq,0))
