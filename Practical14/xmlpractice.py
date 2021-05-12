ac96a39224b848c83d7c5ef6da1828d185965e7b
user <user@userdeMacBook-Air.local>
2021-05-12 20:04:01 +0800

    xml practice

--- Practical14/xmlpractice.py
+++ Practical14/xmlpractice.py
@@ -1,16 +1,22 @@
+#import DOM module
 from xml.dom.minidom import parse
 import xml.dom.minidom
+#parse the file
 DOMTree=xml.dom.minidom.parse('/Users/user/Downloads/go_obo.xml')
+#get the root element
 total=DOMTree.documentElement
+#get a list of elements for different genes
 terms=total.getElementsByTagName('term')
 count1=0
 count2=0
 count3=0
 count4=0
-for n in range (terms.length):
-	if 'DNA' in terms[n].getElementsByTagName('defstr')[0].childNodes[0].data:
+#get the number of childNodes for DNA-related gene ontology terms, reference: https://www.cnblogs.com/zlstg/articles/14095898.html
+for n in range (terms.length):#reference: https://www.cnblogs.com/poloyy/p/12207464.html
+	if 'DNA' in terms[n].getElementsByTagName('defstr')[0].childNodes[0].data:#get the defstr information by only getting the node of defstr and change the type from nodelist to node so that '.data' can be used
 		count1=count1+terms[n].childNodes.length
 
+#repeat for RNA and protein
 for n in range (terms.length):
 	if 'RNA' in terms[n].getElementsByTagName('defstr')[0].childNodes[0].data:
 		count2=count2+terms[n].childNodes.length
@@ -19,6 +25,7 @@ for n in range (terms.length):
 	if 'protein' in terms[n].getElementsByTagName('defstr')[0].childNodes[0].data:
 		count3=count3+terms[n].childNodes.length
 
+#the fourth macromolecule: glycoprotein
 for n in range (terms.length):
 	if 'glycoprotein' in terms[n].getElementsByTagName('defstr')[0].childNodes[0].data:
 		count4=count4+terms[n].childNodes.length
